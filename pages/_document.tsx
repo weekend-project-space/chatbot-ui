fb432d2a41e632196e793ab44189665905416f6c
weekend-project-space <110220257+weekend-project-space@users.noreply.github.com>
2023-04-28 15:16:57 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,16 @@ type Props = DocumentProps & {
   // add custom document props
 };
 
+function createStatistics() {
+ return {__html: `var _hmt = _hmt || [];
+ (function() {
+   var hm = document.createElement("script");
+   hm.src = "https://hm.baidu.com/hm.js?8ba1ec790bd7fe9df8ef26b46ab5a2d0";
+   var s = document.getElementsByTagName("script")[0]; 
+   s.parentNode.insertBefore(hm, s);
+ })();`};
+}
+
 export default function Document(props: Props) {
   const currentLocale =
     props.__NEXT_DATA__.locale ?? i18nextConfig.i18n.defaultLocale;
@@ -14,16 +24,7 @@ export default function Document(props: Props) {
       <Head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="Chatbot UI"></meta>
-        <script>
-          if (!window.location.hostname.includes('localhost')) {
-            var _hmt = _hmt || [];
-            (function () {
-              var hm = document.createElement("script");
-              hm.src = "https://hm.baidu.com/hm.js?8ba1ec790bd7fe9df8ef26b46ab5a2d0";
-              var s = document.getElementsByTagName("script")[0];
-              s.parentNode.insertBefore(hm, s);
-            })();
-          }
+        <script dangerouslySetInnerHTML={createStatistics()}>
         </script>
       </Head>
       <body>
