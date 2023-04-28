cb7aef3176d52d261c19968c4f8723e47603f629
weekend-project-space <110220257+weekend-project-space@users.noreply.github.com>
2023-04-28 15:11:13 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,17 @@ export default function Document(props: Props) {
       <Head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="Chatbot UI"></meta>
+        <script>
+          if (!window.location.hostname.includes('localhost')) {
+            var _hmt = _hmt || [];
+            (function () {
+              var hm = document.createElement("script");
+              hm.src = "https://hm.baidu.com/hm.js?8ba1ec790bd7fe9df8ef26b46ab5a2d0";
+              var s = document.getElementsByTagName("script")[0];
+              s.parentNode.insertBefore(hm, s);
+            })();
+          }
+        </script>
       </Head>
       <body>
         <Main />
